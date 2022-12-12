18e4daff08e0a66f3eab52878a15fceba1562fb9
Leonard <l.vasseur@etudiant.ecolemelies.fr>
2022-12-12 17:14:33 +0100

    Base Ramp, penguin and physics material

---
+++ Assets/SC_Penguin_Behaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SC_Penguin_Behaviour : MonoBehaviour
+{
+    /*private new Rigidbody2D rigidbody2D;
+    private float direction;
+
+    // Update is called once per frame
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void OnMove(InputAction.CallbackContext obj)
+    {
+        direction = obj.ReadValue<value>();
+        this.Transform.rotate.z * direction;
+    }*/
+
+}
