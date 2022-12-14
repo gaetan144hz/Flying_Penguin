b86c9399d27b3664b90ac7c0b2e077e554c1ca87
Leonard <l.vasseur@etudiant.ecolemelies.fr>
2022-12-14 16:03:43 +0100

    Ui Scenes created and scene change script

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    [SerializeField] GameObject loadingScreenPrefab;
+    
+    public void LoadSceneAsync()
+    {
+        StartCoroutine(LoadingScreenCoroutine());
+    }
+
+    private IEnumerator LoadingScreenCoroutine()
+    {
+        var prefab = Instantiate(loadingScreenPrefab);
+        DontDestroyOnLoad(prefab);
+        var sceneLoading = SceneManager.LoadSceneAsync("GameScene"); // permet de charger la scène en décaler
+        sceneLoading.allowSceneActivation = false; // bloque l'affichage de la scène suivante.
+        while (sceneLoading.isDone == false)
+        {
+            if (sceneLoading.progress >= 0.9f) //chargement dans unity est à 90% le rest c'est l'affichage sur le screen
+            {
+                sceneLoading.allowSceneActivation = true; //réautorise l'affichage de la scène game.
+                prefab.SetActive(false);
+            }
+            yield return new WaitForSeconds(1);
+        }
+        //yield return new WaitForSeconds(2);
+        //SceneManager.LoadScene("Game");
+        //yield return null;
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    
+    public void ToControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+       
+    public void ToQuit()
+    {
+        Application.Quit();
+    }
+
+}
