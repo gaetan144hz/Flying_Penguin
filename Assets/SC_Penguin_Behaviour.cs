085e2c495ef636a71d4291c8fe34905277b1e8af
Leonard <l.vasseur@etudiant.ecolemelies.fr>
2022-12-14 11:20:16 +0100

    Ramp and AddForce test

--- Assets/SC_Penguin_Behaviour.cs
+++ Assets/SC_Penguin_Behaviour.cs
@@ -5,20 +5,50 @@ using UnityEngine.InputSystem;
 
 public class SC_Penguin_Behaviour : MonoBehaviour
 {
-    /*private new Rigidbody2D rigidbody2D;
+    private new Rigidbody2D rb2D;
     private float direction;
+    [SerializeField] private float TurningRate;
+    [SerializeField] private float LaunchForce;
+    private bool canRotate;
+    [SerializeField] private float Thrust;
+
+
 
     // Update is called once per frame
     void Awake()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        canRotate = false;
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-
-    public void OnMove(InputAction.CallbackContext obj)
+    private void FixedUpdate()
     {
-        direction = obj.ReadValue<value>();
-        this.Transform.rotate.z * direction;
-    }*/
+        ManageRotation();
+        //rb2D.AddForce(transform.up * Thrust, ForceMode2D.Impulse);
+    }
+
+    private void ManageRotation()
+    {
+        if (!canRotate)
+            return;
+        rb2D.MoveRotation(rb2D.rotation + TurningRate * Time.fixedDeltaTime * direction);
+    }
+
+    public void OnRotate(InputAction.CallbackContext obj)
+    {
+        direction = obj.ReadValue<float>();
+        canRotate = obj.performed;
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ramp"))
+        {
+            rb2D.AddForce(transform.up * LaunchForce, ForceMode2D.Impulse);
+
+        }
+
+    }
 
 }
