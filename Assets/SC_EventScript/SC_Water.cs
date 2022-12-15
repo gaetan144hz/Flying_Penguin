a112917e0c0669f32ed80965b6e83e5ecf61d28a
Leonard <l.vasseur@etudiant.ecolemelies.fr>
2022-12-15 20:07:16 +0100

    SetupForBuild

--- Assets/SC_EventScript/SC_Water.cs
+++ Assets/SC_EventScript/SC_Water.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using Unity.Mathematics;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
